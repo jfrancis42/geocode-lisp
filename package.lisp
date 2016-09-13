c5acf7e13b4d8e75b98519d4caa0eea61808cff0
Jeff Francis <jeff@gritch.org>
2016-09-13 13:42:17 -0700

    A few more changes for SPOT support.

--- package.lisp
+++ package.lisp
@@ -5,11 +5,17 @@
   (:import-from :aviation-formulary
 		:pp
 		:2d-point
+		:point-lat
+		:point-lon
 		:lat
 		:lon)
   (:export :pp
+	   :point-lat
+	   :point-lon
+	   :description
 	   :lookup-location
 	   :lookup-place
+	   :extract-street-address-from-json
 	   :street-address-to-lat-lon
 	   :lat-lon-to-location
 	   :lat-lon-to-street-address))
