047b8ff9dd6a8603f6d111374538dd75436d9e98
Jeff Francis <jeff@gritch.org>
2016-12-08 11:09:37 -0800

    Renamed the example functions more descriptively.

--- example_client.lisp
+++ example_client.lisp
@@ -3,11 +3,11 @@
 
 (creds:load-creds)
 
-(defun what-is-the-lat-lon-of-this-address? ()
+(defun what-is-the-lat-lon-of-the-whitehouse? ()
   "Return an object representing the location of the White House."
   (let ((wh (geocode:street-address-to-lat-lon "1600 Pennsyvania Ave NW, Washington DC" (creds:get-cred "googlegeoapi"))))
     (list (geocode:point-lat wh) (geocode:point-lon wh))))
 
-(defun what-is-the-address-of-this-lat-lon? ()
+(defun what-is-the-address-of-the-whitehouse? ()
   "Return the address of 38.89761,-77.036736."
   (geocode:address (geocode:lat-lon-to-street-address 38.89761 -77.036736 (creds:get-cred "googlegeoapi"))))
